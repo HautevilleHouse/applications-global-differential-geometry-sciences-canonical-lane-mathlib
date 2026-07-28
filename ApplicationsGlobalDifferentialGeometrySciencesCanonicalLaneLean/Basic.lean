aabb171450c7812f21d59fe957efad688282ee39
HautevilleHouse <239203806+HautevilleHouse@users.noreply.github.com>
2026-07-28 12:09:10 -0400

    Canon

---
+++ ApplicationsGlobalDifferentialGeometrySciencesCanonicalLaneLean/Basic.lean
@@ -0,0 +1,2 @@
+import ApplicationsGlobalDifferentialGeometrySciencesCanonicalLaneLean.GateLemmas
+import ApplicationsGlobalDifferentialGeometrySciencesCanonicalLaneLean.FinalTheorem
---
+++ applications_global_differential_geometry_sciences_canonical_lane_mathlib/FinalTheorem.lean
@@ -0,0 +1,9 @@
+import applications_global_differential_geometry_sciences_canonical_lane_mathlib.GateLemmas
+namespace HautevilleHouse
+namespace applications_global_differential_geometry_sciences_canonical_lane_mathlib
+structure ConstrainedTheoremClosure where
+  admissible :: intro :: constraint :: consequence :: proof ::
+theorem canonical : ConstrainedTheoremClosure :=
+  ConstrainedTheoremClosure.intro
+end applications_global_differential_geometry_sciences_canonical_lane_mathlib
+end HautevilleHouse
